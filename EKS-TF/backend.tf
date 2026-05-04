cabbeb71ca0f36680ed8eb90d2c1879eff1ad372
Linziee-pixel <tranlinh4795@gmail.com>
2026-05-04 13:26:15 +0700

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "linhtt-s3-bucket-296725355870-ap-southeast-1-an"
     region       = "ap-southeast-1"
-    key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
+    key          = "Tetris-Project-Devops/EKS-TF/terraform.tfstate"
     use_lockfile = true
     encrypt      = true
   }
