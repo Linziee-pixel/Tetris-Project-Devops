284749aa508c0524a0635a16c3716317aa05d3ad
Linh Tran <tranlinh4795@gmail.com>
2026-05-05 10:39:07 +0700

    Update S3 bucket name in backend.tf for EKS and Jenkins configurations

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "linhtt-s3-bucket-296725355870-ap-southeast-1-an"
+    bucket       = "linhtt1-s3-bucket-296725355870-ap-southeast-1-an"
     region       = "ap-southeast-1"
     key          = "Tetris-Project-Devops/EKS-TF/terraform.tfstate"
     use_lockfile = true
